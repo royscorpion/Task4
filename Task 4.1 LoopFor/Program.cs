32e153f5687e224c92e3f9f2a253827b6be060c4
Виктор Парыгин <paryginva@live.com>
2021-11-20 01:11:25 +0500

    Добавьте файлы проекта.

---
+++ Task 4.1 LoopFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вывод квадрата введенного целого числа, вычисленного по формуле N^2 = 1 + 3 + 5 + ... + (2*N-1).
+            Console.Write("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int Sum = 0;
+            for (int i = 1; i <= (2 * number - 1); i += 2)
+            {
+                Sum += i;
+                Console.WriteLine("Текущее значение суммы = {0}", Sum);
+            }
+            Console.WriteLine("Квадрат введенного числа {0} равен {1}",number,Sum);
+            
+            Console.WriteLine("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 4.2 LoopDo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._2_LoopDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определение наибольшего количества положительных либо отрицательных целых чисел в введенной последовательности
+            Console.WriteLine("Введите положительные и отрицательные целые числа в произвольной последовательности");
+            Console.WriteLine("Для завершения последовательности введите 0");
+            int number, negativeNumber = 0, positiveNumber = 0;
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number < 0)
+                {
+                    negativeNumber++;
+                }
+                else if (number>0)
+                {
+                    positiveNumber++;
+                }
+            } while (number != 0);
+
+            Console.WriteLine("Введено {0} положительных и {1} отрицательных чисел.",positiveNumber,negativeNumber);
+            Console.WriteLine(negativeNumber > positiveNumber ?"Количество отрицательных чисел больше, чем положительных":"Количество положительных чисел больше, чем отрицательных");
+
+            Console.WriteLine("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+}
